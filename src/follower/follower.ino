f6ab7770800555e8a0590c045f892979df39eb5c
Melih <melihboyun@hotmail.com>
2025-12-01 23:11:50 -0800

    Motor control, sensor integration, buzzer functionality added.

--- src/follower/follower.ino
+++ src/follower/follower.ino
@@ -1,13 +1,44 @@
 #include <Wire.h>
+#include <ESP32Servo.h>
 
+Servo myServo;
+
+#define SERVO_PIN 18   
+#define PIR_PIN   4    
 #define ADDR 8
+#define BUZZER_PIN 19
 
-void onReceive(int len) {
+int lastState = LOW;
+int pirState = digitalRead(PIR_PIN);
+
+
+void moveServo_withSensor(int pirState, int lastState) {
+// Move servo instantly when state changes
+  int currentAngle = 0;
+  if (pirState != lastState) {
+    lastState = pirState;
+    if (pirState == HIGH) {
+      currentAngle = 360;
+      myServo.write(currentAngle);
+    }else{
+      currentAngle = 0;
+      myServo.write(currentAngle);
+    }
+  }
+}
+
+void onReceive(int len, int pirState, int lastState) {
   while (Wire.available()) {
     int command = Wire.read();
     
     if (command == 1) { //correct password
-
+      moveServo_withSensor(pirState);
+    }else{
+      if(pirState == HIGH){
+        digitalWrite(BUZZER_PIN, HIGH); 
+        delay(1000);                   // Buzzer on for 1 second
+        digitalWrite(BUZZER_PIN, LOW);  
+      }
     }
   }
 }
@@ -16,6 +47,13 @@ void setup() {
   Serial.begin(115200);
   Wire.onReceive(onReceive);
   Wire.begin(ADDR); 
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  //Servo and Sensor setup
+  myServo.attach(SERVO_PIN, 500, 2400);
+  currentAngle = 0;
+  myServo.write(currentAngle);
+  pinMode(PIR_PIN, INPUT);
 }
 
 
